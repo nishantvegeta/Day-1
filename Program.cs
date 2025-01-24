db1c522013b224c62a6f955fdc34f12d4b0db04e
Nishant Kumpakha <nishantkumpakha@Nishants-MacBook-Air.local>
2025-01-24 07:37:42 +0545

    list and remove duplicates

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿List<int> list = new List<int>();
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(4);
+list.Add(5);
+list.Add(6);
+list.Add(7);
+list.Add(8);
+list.Add(9);
+list.Add(10);
+
+foreach (int i in list)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+int max = list[0];
+int min = list[0];
+
+foreach (int i in list)
+{
+    if (i > max)
+    {
+        max = i;
+    }
+    if (i < min)
+    {
+        min = i;
+    }
+}
+Console.WriteLine("Max: " + max);
+Console.WriteLine("Min: " + min);
+
+
---
+++ duplicateremove/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+    {
+        static List<string> RemoveDuplicate(List<string> list)
+        {
+            List<string> duplicate = new List<string>();
+
+            foreach (string i in list)
+            {
+                if (!duplicate.Contains(i))
+                {
+                    duplicate.Add(i);
+                }
+            }
+
+            return duplicate;
+        }
+
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string> { "apple", "banana", "vegeta", "nishant", "banana", "grape", "vegeta", "laptop", "apple" };
+
+            Console.WriteLine("Original List:");
+            foreach (string str in list)
+            {
+                Console.WriteLine(str);
+            }
+
+            List<string> uniqueList = RemoveDuplicate(list);
+
+            Console.WriteLine("\nList with Duplicates Removed:");
+            foreach (string str in uniqueList)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
